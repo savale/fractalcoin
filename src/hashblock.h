c2f61da72b49004eba7784e2aab92c5386f98db7
Sander van Leeuwen <djsavale@hotmail.com>
2014-07-01 11:52:15 +0200

    Update hashblock.h

--- src/hashblock.h
+++ src/hashblock.h
@@ -50,12 +50,20 @@ inline uint256 Hash9(const T1 pbegin, const T1 pend)
     sph_simd512_init(&ctx_simd);
     sph_simd512 (&ctx_simd, static_cast<const void*>(&hash[0]), 64);
     sph_simd512_close(&ctx_simd, static_cast<void*>(&hash[1]));
+    
+    sph_shavite512_init(&ctx_shavite);
+    sph_shavite512(&ctx_shavite, static_cast<const void*>(&hash[1]), 64);
+    sph_shavite512_close(&ctx_shavite, static_cast<void*>(&hash[2]));
+    
+    sph_simd512_init(&ctx_simd);
+    sph_simd512 (&ctx_simd, static_cast<const void*>(&hash[2]), 64);
+    sph_simd512_close(&ctx_simd, static_cast<void*>(&hash[3]));
 
     sph_echo512_init(&ctx_echo);
-    sph_echo512 (&ctx_echo, static_cast<const void*>(&hash[1]), 64);
-    sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[2]));
+    sph_echo512 (&ctx_echo, static_cast<const void*>(&hash[3]), 64);
+    sph_echo512_close(&ctx_echo, static_cast<void*>(&hash[4]));
 
-    return hash[2].trim256();
+    return hash[4].trim256();
 }
 
 
